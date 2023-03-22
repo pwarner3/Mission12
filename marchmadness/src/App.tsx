8b74f88f156c37d20b8fee20a441785a0df4471a
Parker Warner <plouwarner@gmail.com>
2023-03-22 13:06:32 -0600

    comment

--- marchmadness/src/App.tsx
+++ marchmadness/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { data } from './teams'
+import { data } from './teams'//File with data
 
 //Component for heading and description
 function Welcome() {
